e9855fa6086e3c15fbc75bbed1744b67e996ad1a
Your Name <you@example.com>
2025-12-02 16:29:21 +0100

    app.py

--- app.py
+++ app.py
@@ -9,7 +9,9 @@ import random as r
 import time as t
 
 class BetterLift:
-	def __init__(self, liftcount, traveltime):
+	def __init__(self, conn, liftcount, traveltime):
+		
+		self.conn = conn
 		self.queue = []
 		self.liftcount = liftcount
 		self.traveltime = traveltime
@@ -18,9 +20,6 @@ class BetterLift:
 	def printLifts(self):
 		for lift in self.lifts:
 			print(f'current_level: {lift["current_level"]} levels:lift["queue"]')
-	def interface(self):
-		try: self.addLevel(int(input("Enter your target level:\n")))
-		except Exception as e: print(e);
 	def getShortestQueue(self):
 		qLengths = [len(lift['queue']) for lift in self.lifts]
 		return (qLengths.index(min(qLengths)))
@@ -34,29 +33,55 @@ class BetterLift:
 		return (sum(allqlengths) == 0)
 	def runLifts(self):
 		arrivals = 0
-		while not self.getAllQueuesEmpty():
-			t.sleep(self.traveltime)
-			for i, lift in enumerate(self.lifts):
-				if not len(lift['queue']): continue
-				if lift['queue'][0] < lift['current_level']:
-					self.lifts[i]['current_level'] -= 1
-					print(f'Moving Lift #{i} down to {self.lifts[i]["current_level"]}')
-				elif lift['queue'][0] > lift['current_level']:
-					self.lifts[i]['current_level'] += 1
-					print(f'Moving Lift #{i} up to {self.lifts[i]["current_level"]}')
-				else:
-					arrivals += 1
-					self.lifts[i]['queue'].pop(0)
-					print(f'Lift #{i} has arrived at', self.lifts[i]['current_level'], f'arrival #{arrivals}')
+		#while not self.getAllQueuesEmpty():
+		t.sleep(self.traveltime)
+		for i, lift in enumerate(self.lifts):
+			print("Doing Lift", i)
+			if not len(lift['queue']): continue
+			if lift['queue'][0] < lift['current_level']:
+				self.lifts[i]['current_level'] -= 1
+				print(f'Moving Lift #{i} down to {self.lifts[i]["current_level"]}')
+			elif lift['queue'][0] > lift['current_level']:
+				self.lifts[i]['current_level'] += 1
+				print(f'Moving Lift #{i} up to {self.lifts[i]["current_level"]}')
+			else:
+				arrivals += 1
+				self.lifts[i]['queue'].pop(0)
+				print(f'Lift #{i} has arrived at', self.lifts[i]['current_level'], f'arrival #{arrivals}')
 
 	def main(self):
-		self.interface()
-		#for i in range(100):
-		#	newlevel = r.randint(-10, 30)
-		#	self.addLevel(newlevel)
-		self.runLifts()
+		newlevel = self.conn.recv()
+		print("Received new level", newlevel)
+		self.addLevel(newlevel)
+		while not self.getAllQueuesEmpty(): self.runLifts()
 		
+import multiprocessing
+import os
+
+def childProcess(conn):
+	bl = BetterLift(conn, 4, .1)
+	while 1: bl.main()
+	conn.close()
+
+def interface():
+	newlevel = input("Enter your target level:\n")
+	if newlevel != 'exit':
+		try: return (int(newlevel))
+		except Exception as e: print(e);
+	return 'exit'
+
+def LiftManager():
+	parent_conn, child_conn = multiprocessing.Pipe()
+	
+	p = multiprocessing.Process(target=childProcess, args = (child_conn,))
+	p.start()
+	while 1:
+		newlevel = interface()
+		if newlevel == 'exit': return
+		parent_conn.send(newlevel)
+	#parent_conn.send('exit')
+	p.join()
+	parent_conn.close()
 
 if __name__ == '__main__':
-	bl = BetterLift(4, .05)
-	bl.main()
+	LiftManager()
